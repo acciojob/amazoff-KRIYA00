8861dd494a7e3d1c629ccf24d23b17c891531873
ysi <yshouldi@gmail.com>
2023-09-23 03:21:19 +0530

    8

--- src/main/java/com/driver/OrderRepository.java
+++ src/main/java/com/driver/OrderRepository.java
@@ -138,20 +138,20 @@ public class OrderRepository {
 
              }
          }
-        if(partnerOrderdB.containsKey(dpid))
+     /*   if(partnerOrderdB.containsKey(dpid))
         {
             partnerOrderdB.remove(dpid);
         }
         if(deliverydB.containsKey(dpid))
             deliverydB.remove(dpid);
-
-       /* List<String> ordersOfPartner = partnerOrderdB.get(dpid);
+*/
+        List<String> ordersOfPartner = partnerOrderdB.get(dpid);
         for(String orderId: ordersOfPartner){
             orderpartnerdB.remove(orderId);
         }
         partnerOrderdB.remove(dpid);
        deliverydB.remove(dpid);
-       */
+
 
     }
     public void deleteOrderById(String orid)
